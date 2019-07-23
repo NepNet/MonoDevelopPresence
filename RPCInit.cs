1ad66d4317274b1103fefc30826ce924152e7f38
Baka-chan <catalinbarbuta14@gmail.com>
2019-07-23 20:34:30 +0300

    Initial commit
    Usable but not very good

---
+++ RPCInit.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using MonoDevelop.Components.Commands;
+
+namespace MonoDevelopRPC
+{
+	[MonoDevelop.Ide.Extensions.StartupHandlerExtension]
+	public class RPCInit : CommandHandler
+	{
+		static DiscordRPC.RichPresence presence = RPC_Controller.presence;
+		static string ProjectName;
+		static string FileName;
+
+		static string LargeImageText
+		{
+			get
+			{
+				return $"Project: {ProjectName}\nFile: {FileName}";
+			}
+		}
+		protected override void Run()
+		{
+			var workspace = MonoDevelop.Ide.IdeApp.Workspace;
+
+			workspace.SolutionUnloaded += Workspace_SolutionUnloaded;
+		
+			MonoDevelop.Ide.IdeApp.ProjectOperations.CurrentProjectChanged += ProjectOperations_CurrentProjectChanged;
+			MonoDevelop.Ide.IdeApp.Workbench.ActiveDocumentChanged += Workbench_ActiveDocumentChanged;
+
+			Task.Run(() => RPC_Controller.StartRPC());
+		}
+
+		void WriteLog(string filename, string value)
+		{
+			string path = Environment.SpecialFolder.Personal + "/" + filename + ".txt";
+
+			if (!File.Exists(path))
+			{
+				File.Create(path).Dispose();
+
+				using (TextWriter tw = new StreamWriter(path))
+				{
+					tw.WriteLine(value);
+				}
+
+			}
+			else if (File.Exists(path))
+			{
+				using (TextWriter tw = new StreamWriter(path))
+				{
+					tw.WriteLine(value);
+				}
+			}
+		}
+
+		void Workbench_ActiveDocumentChanged(object sender, EventArgs e)
+		{
+			var ActiveDocument = MonoDevelop.Ide.IdeApp.Workbench.ActiveDocument;
+			if (ActiveDocument != null)
+			{
+				presence.State = $"Editing {Path.GetFileName(ActiveDocument.Name)}";
+				FileName = Path.GetFileName(ActiveDocument.Name);
+				presence.Assets.LargeImageText = LargeImageText;
+			}
+		}
+
+		void ProjectOperations_CurrentProjectChanged(object sender, MonoDevelop.Projects.ProjectEventArgs e)
+		{
+			if (e.Project != null)
+			{
+				presence.Details = e.Project.Name;
+				ProjectName = e.Project.Name;
+				presence.Assets.LargeImageText = LargeImageText;
+			}
+		}
+
+		void Workspace_SolutionUnloaded(object sender, MonoDevelop.Projects.SolutionEventArgs e)
+		{
+			presence.Details = "Home screen";
+			presence.State = "";
+			presence.Assets.LargeImageText = "MonoDevelop";
+		}
+	}
+}
---
+++ RPC_Controller.cs
@@ -0,0 +1,100 @@
+﻿using DiscordRPC;
+using DiscordRPC.Message;
+using System;
+using System.Text;
+using System.Threading;
+using Mono.Addins;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide.Commands;
+
+namespace MonoDevelopRPC
+{
+	public static class RPC_Controller
+	{
+		private static DiscordRpcClient client;
+		private static bool isRunning = true;
+
+		internal static RichPresence presence = new RichPresence()
+		{
+			Details = "Home screen",
+			//State = "Editing 'something.wtf'",
+			Assets = new Assets()
+			{
+				LargeImageKey = "logo",
+				LargeImageText = "MonoDevelop",
+				SmallImageKey = "logo",
+				SmallImageText = "broken"
+			}
+		};
+
+		[CommandHandler("StartRPC")]
+		public static void StartRPC()
+		{
+			using (client = new DiscordRpcClient("595335536802267187", pipe: -1, autoEvents: true))
+			{
+				client.RegisterUriScheme();
+
+				client.OnReady += OnReady;                                      //Called when the client is ready to send presences
+				client.OnClose += OnClose;                                      //Called when connection to discord is lost
+				client.OnError += OnError;                                      //Called when discord has a error
+				Console.WriteLine(MonoDevelop.Ide.IdeApp.Workspace.Name);
+
+				//Give the game some time so we have a nice countdown
+				presence.Timestamps = new Timestamps()
+				{
+					Start = DateTime.UtcNow,
+					//End = DateTime.UtcNow + TimeSpan.FromSeconds(15)
+				};
+
+				//Set some new presence to tell Discord we are in a game.
+				// If the connection is not yet available, this will be queued until a Ready event is called, 
+				// then it will be sent. All messages are queued until Discord is ready to receive them.
+				client.SetPresence(presence);
+
+				//Initialize the connection. This must be called ONLY once.
+				//It must be called before any updates are sent or received from the discord client.
+				client.Initialize();
+
+				//Start our main loop. In a normal game you probably don't have to do this step.
+				// Just make sure you call .Invoke() or some other dequeing event to receive your events.
+
+				while (client != null && isRunning)
+				{
+					Thread.Sleep(25);
+
+
+
+					client.SetPresence(presence);
+				}
+			}
+
+		}
+
+		#region State Events
+		private static void OnReady(object sender, ReadyMessage args)
+		{
+			//This is called when we are all ready to start receiving and sending discord events. 
+			// It will give us some basic information about discord to use in the future.
+
+			//DEBUG: Update the presence timestamp
+			presence.Timestamps = Timestamps.Now;
+
+			//It can be a good idea to send a inital presence update on this event too, just to setup the inital game state.
+			Console.WriteLine("On Ready. RPC Version: {0}", args.Version);
+
+		}
+		private static void OnClose(object sender, CloseMessage args)
+		{
+			//This is called when our client has closed. The client can no longer send or receive events after this message.
+			// Connection will automatically try to re-establish and another OnReady will be called (unless it was disposed).
+			Console.WriteLine("Lost Connection with client because of '{0}'", args.Reason);
+		}
+		private static void OnError(object sender, ErrorMessage args)
+		{
+			//Some error has occured from one of our messages. Could be a malformed presence for example.
+			// Discord will give us one of these events and its upto us to handle it
+			Console.WriteLine("Error occured within discord. ({1}) {0}", args.Message, args.Code);
+		}
+		#endregion
+	}
+}
